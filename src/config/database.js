378766b0d63f8f4a95a90f56737064ef7232f0ca
[Carllux Vinicius] <[carlos.vinicius-@hotmail.com]>
2023-02-17 16:45:24 -0300

    First commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     dialect: 'mssql',
-    host: '192.168.3.225',
+    host: '192.168.3.187',
     username: 'sa',
-    password: 'sysadm',
+    password: 'desenvolvimento#@2023',
     database: 'BDAdm',
     define: {
         timestamps: true,
