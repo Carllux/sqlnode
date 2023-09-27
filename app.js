036b8cd7c19022c1ac0f8ec2f7577e034f8b4d0e
Carllux <82111083+Carllux@users.noreply.github.com>
2023-09-27 14:51:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,17 @@ import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
 import express from 'express';
+import AppError from './src/utils/appError.js';
 import usuarioRoutes from './src/routes/usuarioRoutes.js';
 import tokenRoutes from './src/routes/tokenRoutes.js';
 import pedidoRoutes from './src/routes/pedidoRoutes.js';
+import path from 'path';
+import url from 'url';
+import globalErrorHandler from './src/controllers/errorController.js';
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 // Versão um da API
 const v1Url = `${process.env.V1_URL}`;
 // const routes = require('./routes');
@@ -43,17 +50,26 @@ class App {
   middlewares() {
     this.app.use(helmet());
     this.app.use(cors(corsOptions));
-    this.app.use(morgan('tiny'));
-    //this.app.use('/public/', express.static(resolve(__dirname, 'public')));
+    if (process.env.NODE_ENV === 'development') {
+      this.app.use(morgan('dev'));
+    }
+    // analisar 
+    this.app.use('/public/', express.static(resolve(__dirname, 'public')));
     this.app.use(express.json());
     // this.app.use(multer().any());
     this.app.use(express.urlencoded({ extended: true }));
+    // Para tratamento de rotas não planejadas
   }
 
   routes() {
     this.app.use(`${v1Url}/usuarios`, usuarioRoutes);
     this.app.use(`${v1Url}/login`, tokenRoutes);
     this.app.use(`${v1Url}/pedidos`, pedidoRoutes);
+    this.app.all('*', (req, res, next) => {
+      next(new AppError(`Não foi possível encontrar a seguinte rota ${req.originalUrl} neste servidor.`, 404))
+    })
+
+    this.app.use(globalErrorHandler)
   }
 }
 
